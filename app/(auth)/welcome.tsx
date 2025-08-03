62d0cb4dfb63fa707f450dcd68dd91fdd330e952
Shekeeb <shekeebmohammed10@gmail.com>
2025-08-03 12:29:44 +0530

    Welcome screen completed

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,82 @@
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '@/components/ScreenWrapper'
+import Typo from '@/components/Typo'
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import { verticalScale } from '@/utils/style'
+import welcomeImage from "../../assets/images/welcome.png"
+import  Button  from '../../components/Button'
+import Animated, { FadeIn, FadeInDown } from "react-native-reanimated"
+
+const welcome = () => {
+    return (
+        <ScreenWrapper>
+            <View style={styles.container}>
+                <View>
+                    <TouchableOpacity style={styles.loginButton}>
+                        <Typo style={{ fontWeight: "500" }}>Sign In</Typo>
+                    </TouchableOpacity>
+
+                    <Animated.Image entering={FadeIn.duration(2000)} source={welcomeImage} style={styles.welcomeImage} resizeMode='contain' />
+                </View>
+
+                <View style={styles.footer}>
+                    <Animated.View entering={FadeInDown.duration(1000).springify().damping(12)} style={{ alignItems: "center" }}>
+                        <Typo size={30} fontWeight={"800"}>Always take control</Typo>
+                        <Typo size={30} fontWeight={"800"}>of your finances</Typo>
+                    </Animated.View>
+                   
+                    <Animated.View entering={FadeInDown.duration(100).delay(500).springify().damping(12)} style={{ alignItems: "center", gap: 2 }}>
+                            <Typo size={17} color={colors.textLight}>Finances must be arranged to a set a better</Typo>
+                            <Typo size={17} color={colors.textLight}>lifestyle in future</Typo>
+                    </Animated.View>
+
+                    <Animated.View entering={FadeInDown.duration(200).delay(1000).springify().damping(12)} style={styles.buttonContainer}>
+                        <Button>
+                            <Typo size={22} color={colors.neutral900} fontWeight={"600"}>Get Started</Typo>
+                        </Button>
+                    </Animated.View>
+
+                </View>
+
+            </View>
+        </ScreenWrapper>
+    )
+}
+
+export default welcome
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "space-between",
+        paddingTop: spacingY._7
+    },
+    welcomeImage: {
+        width: "100%",
+        height: verticalScale(300),
+        alignSelf: "center",
+        marginTop: verticalScale(100)
+    },
+    loginButton: {
+        alignSelf: "flex-end",
+        marginRight: spacingX._20
+    },
+    footer: {
+        backgroundColor: colors.neutral900,
+        alignItems: "center",
+        paddingTop: verticalScale(30),
+        paddingBottom: verticalScale(45),
+        gap: spacingY._20,
+        shadowColor: "red",
+        shadowOffset: { width: 0, height: -10 },
+        elevation: 10,
+        shadowRadius: 25,
+        shadowOpacity: 0.15
+    },
+    buttonContainer: {
+        width: "100%",
+        paddingHorizontal: spacingX._25
+    },
+
+})
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, View ,Image} from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import splashImage from "../assets/images/splashImage.png"
 import { colors } from '@/constants/theme'
+import { useRouter } from 'expo-router'
 
 const index = () => {
+
+  const router=useRouter();
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      router.push('/(auth)/welcome');
+    },3000)
+  },[])
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} resizeMode="contain" source={splashImage}/>
